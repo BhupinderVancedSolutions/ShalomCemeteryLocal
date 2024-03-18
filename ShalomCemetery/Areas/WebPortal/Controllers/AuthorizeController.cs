6a3a973c00d88958afc025b49eb9b765fa0df937
bhupinder <bhupinderkumar@vancedsolutions.com>
2024-03-18 16:45:09 +0530

    Initial Commit

---
+++ ShalomCemetery/Areas/WebPortal/Controllers/AuthorizeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShalomCemetery.Areas.WebPortal.Controllers
+{
+    public class AuthorizeController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View("~/Areas/WebPortal/Views/Authorize/Index.cshtml");
+        }
+    }
+}
---
+++ ShalomCemetery/Areas/WebPortal/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol.Plugins;
+
+namespace ShalomCemetery.Areas.WebPortal.Controllers
+{
+    public class BaseController : Controller
+    {
+        private ISender _mediator;
+        protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetService<ISender>();
+    }
+}
